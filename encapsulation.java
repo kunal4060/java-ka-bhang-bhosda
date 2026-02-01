7f9c5da512026c3b0f6c3c13987a3bc6fdc09a6d
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-01 22:43:06 +0530

    vahi defination

--- encapsulation.java
+++ encapsulation.java
@@ -7,5 +7,6 @@ public class encapsulation {
     //data hinding can be achive using encapsulation
     //we can make it private or protected
     //single entity me data and function ko rakhna encapsulation kehlata hai
+    //encapsulation ne hamne try kiya hai ye ek class me kiya jata hai jaha pr ham data hide kr skte hai aur us data ko use bhi kr sakte hai set and get keyword ke sath
     
 }
