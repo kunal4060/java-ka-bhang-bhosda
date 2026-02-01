d638e6ee7c9ee1c0f9d0d635af810f84147489f8
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-01 22:40:11 +0530

    encapsulation is theory part

--- encapsulation.java
+++ encapsulation.java
@@ -1,4 +1,11 @@
 public class encapsulation {
     // encapsulation is the wrapping up of data members and member functions into a single unit called class
+    //capusule kya hoti hai davai kuch bahra hota hai
+    // data hiding is the main concept of encapsulation
+    //data and attribute in single enttity callled encapsulation
+    // it is one of the four pillars of OOPS
+    //data hinding can be achive using encapsulation
+    //we can make it private or protected
+    //single entity me data and function ko rakhna encapsulation kehlata hai
     
 }
