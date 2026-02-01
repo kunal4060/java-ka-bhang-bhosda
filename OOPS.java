7d7c72f81068ff9346a7e4bcbd33e6a7e4138f79
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-01 21:55:13 +0530

    Initial commit: Practicing GitHub upload

---
+++ OOPS.java
@@ -0,0 +1,42 @@
+public class OOPS{
+    public static void main(String arg[]){
+        // Pen pentonic = new Pen();
+        // pentonic.setcolour("pink");// indirect access
+        // pentonic.settip(0.5f);// indirect access
+        // System.out.println(pentonic.colour);// direct access
+        // System.out.println(pentonic.tip);// direct access
+        // pentonic.colour = "blue"; // direct access
+        // System.out.println(pentonic.colour);
+
+
+    }
+    
+}
+
+// class Pen{
+//     String colour;
+//     float tip;
+//     void setcolour(String newcolour){
+//         colour = newcolour;
+//     }
+//     void settip(float newtip){
+//         tip = newtip;
+//     }
+    
+// }
+
+// class student{
+//     String name;
+//     int age;
+//     int rollno;
+
+//     void setname(String newname){
+//         name = newname;
+//     }
+//     void setage(int newage){
+//         age = newage;
+//     }
+//     void setrollno(int newrollno){
+//         rollno = newrollno;
+//     }
+// }
