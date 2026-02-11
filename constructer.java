030f1480bb182191bb85c855f1969f935fcf48a3
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-11 09:33:27 +0530

    yes

---
+++ constructer.java
@@ -0,0 +1,9 @@
+//constructer is special typwe odf function which is used to initialize the objects of a class
+// it is called automatically when an object of a class is created
+//pen p1 = new pen();
+//example below
+//constructer ko ek hi bar call krte hai jab ham object create krte hai   
+class student{
+    String name;
+    int rollno;
+}
---
+++ inputreentry.java
@@ -0,0 +1,7 @@
+public class inputreentry {while(true) {
+   take input
+   if(condition satisfied)
+       break;
+}
+    
+}
